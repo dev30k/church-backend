0f4227b586074526159601ad6f1ed9a80cd92861
Lemuria <66430325+dev30k@users.noreply.github.com>
2023-02-13 16:55:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import church from "./src/routes/church.js";
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require('express');
+const bodyParser = require('body-parser');
+const {router} = require("./src/routes/church") ;
+const cors = require('cors');
+const dotenv =  require('dotenv');
 
 dotenv.config()
 
-const CORS = cors();
 const app = express();
-const PORT = process.env.PORT ||5000;
-const HOSTNAME = '127.0.0.1'
+const PORT = 4000;
+const HOSTNAME = 'localhost'
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(CORS);
 app.use(bodyParser.json());
-app.use('/church_sys/api/v1/', church);
+app.use('/church_sys/api/v1/', router);
 app.get('/', (req, res) => res.send('Congratulations you have reached the Hearts Blaze API!!!'));
 
 app.listen(PORT, () => console.log(`listening on Port 'http://${HOSTNAME}:${PORT}`));
